a65cdf9a929bc72f78daedf0bfda2288ee474c8d
Eric <marquezeric@hotmail.com>
2022-07-04 16:48:46 -0500

    Inicio Estructuras

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,12 @@
+//  IDS-Eric Márquez Salas
+//  Estructuras de Control
+
+const puntaje = 5000;
+
+// --- Se compara si es igual una condición
+if (puntaje == 1000) {
+    console.log('Si es igual...');
+}
+else {
+    console.log('No es igual');
+}
